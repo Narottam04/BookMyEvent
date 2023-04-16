eb98a7c1bb3c763ce4b32f1720ffeef09995582f
Zaid Khan <zk76159@gmail.com>
2023-04-16 22:25:13 +0530

    Added event details

---
+++ frontend/src/Utils/ongoingevents.js
@@ -0,0 +1,49 @@
+const data = [
+    {
+        title: "Hackathon",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+        poster: "/5.jpeg",
+        start: "01-01-2023",
+        end: "01-01-2023",
+        seats: "50",
+        type: "Offline"
+    },
+    {
+        title: "Into to web",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+        poster: "/6.jpeg",
+        start: "01-01-2023",
+        end: "01-01-2023",
+        seats: "/harishchandragad",
+        type: "virtual"
+    },
+    {
+        title: "Blockchain",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+        poster: "/4.jpeg",
+        start: "01-01-2023",
+        end: "01-01-2023",
+        seats: "/harishchandragad",
+        type: "virtual"
+    },
+    {
+        title: "Blockchain",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+        poster: "/4.jpeg",
+        start: "01-01-2023",
+        end: "01-01-2023",
+        seats: "/harishchandragad",
+        type: "virtual"
+    },
+    {
+        title: "Blockchain",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+        poster: "/4.jpeg",
+        start: "01-01-2023",
+        end: "01-01-2023",
+        seats: "/harishchandragad",
+        type: "virtual"
+    },
+]
+
+export default data
